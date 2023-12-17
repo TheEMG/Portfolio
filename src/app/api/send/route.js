0742ecf1f52cf673523f22520dacb601a3ce3521
TheEMG <124335726+TheEMG@users.noreply.github.com>
2023-12-17 11:46:14 -0600

    env changes

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -3,14 +3,14 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
+const personalEmail=process.env.PERSONAL;
 
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
       from: fromEmail,
-      to: [fromEmail, email],
+      to: [personalEmail, email],
       subject: subject,
       react: (
         <>
